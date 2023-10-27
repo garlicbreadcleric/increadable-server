46c5bc76bc9c11d7324823358c2f5adea13f7635
Daniil Kolesnichenko <garlicbreadcleric@yahoo.com>
2023-10-28 01:57:34 +0400

    Increase uploaded file size limit to 200MB

--- src/modules/document/document.controller.ts
+++ src/modules/document/document.controller.ts
@@ -1,12 +1,11 @@
 import { Controller, Get, Param, Post, UploadedFile, UseInterceptors } from "@nestjs/common";
 import { FileInterceptor } from "@nestjs/platform-express";
 import { ApiBody, ApiConsumes, ApiCreatedResponse, ApiOkResponse, ApiTags } from "@nestjs/swagger";
+import { plainToInstance } from "class-transformer";
 
 import { FileService } from "./document.service";
 import { UploadFileDto } from "./dto/upload-file.dto";
 import { DocumentDto } from "./dto/document.dto";
-import { plainToInstance } from "class-transformer";
-import { DocumentMetadata } from "./document-metadata.type";
 
 @ApiTags("Document")
 @Controller("v1/documents")
@@ -15,30 +14,18 @@ export class FileController {
 
   @Get("/:documentId")
   @ApiOkResponse({ type: DocumentDto })
-  async findById(@Param("documentId") documentId): Promise<DocumentDto> {
+  async findById(@Param("documentId") documentId: string): Promise<DocumentDto> {
     const document = await this.fileService.findById(documentId);
     return plainToInstance(DocumentDto, document);
-    // return {
-    //   id: document.id,
-    //   type: document.type,
-    //   previewFileUrl: document.previewFileUrl!,
-    //   metadata: plainToInstance(DocumentMetadata, document.metadata),
-    // };
   }
 
   @Post()
   @ApiConsumes("multipart/form-data")
   @ApiBody({ type: UploadFileDto })
   @ApiCreatedResponse({ type: DocumentDto })
-  @UseInterceptors(FileInterceptor("file"))
+  @UseInterceptors(FileInterceptor("file", { limits: { fieldSize: 200_000_000 /* 200MB */ } }))
   async upload(@UploadedFile() file: Express.Multer.File): Promise<DocumentDto> {
     const document = await this.fileService.upload(file);
     return plainToInstance(DocumentDto, document);
-    // return {
-    //   id: document.id,
-    //   type: document.type,
-    //   previewFileUrl: document.previewFileUrl!,
-    //   metadata: plainToInstance(DocumentMetadata, document.metadata),
-    // };
   }
 }
